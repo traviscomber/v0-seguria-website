eb8b97215435b63a7ead67be2d18416a00de7183
traviscomber <n3uralia@gmail.com>
2026-07-28 18:18:37 -0400

    feat(wildlife): expose review center in admin

---
+++ app/admin/vision/page.tsx
@@ -0,0 +1,7 @@
+import { WildlifeMediaReviewCenter } from '@/components/wildlife-media-review-center'
+
+export const dynamic = 'force-dynamic'
+
+export default function WildlifeVisionReviewPage() {
+  return <WildlifeMediaReviewCenter />
+}
